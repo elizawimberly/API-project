e719d8ae8655abf035f683e4bdbbe9f9f43264d5
Eliza <elizawimberly@gmail.com>
2022-08-26 09:45:43 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,37 +1,19 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
-const { setTokenCookie, restoreUser } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const { restoreUser } = require("../../utils/auth.js");
 
 // All the URLs of the routes in the api router will be prefixed with /api.
 
 router.use(restoreUser);
 
-//routes used to test authentication
-// router.post("/test", function (req, res) {
-//   res.json({ requestBody: req.body });
-// });
+router.use("/session", sessionRouter);
 
-// //this route will test the setTokenCookie function by getting the demo user and calling setTokenCookie.
-// router.get("/set-token-cookie", async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: "Demo-lition",
-//     },
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// });
+router.use("/users", usersRouter);
 
-// //GET /api/set-token-cookie
-// router.get("/restore-user", (req, res) => {
-//   return res.json(req.user);
-// });
-
-// // GET /api/require-auth
-// const { requireAuth } = require("../../utils/auth.js");
-// router.get("/require-auth", requireAuth, (req, res) => {
-//   return res.json(req.user);
-// });
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
