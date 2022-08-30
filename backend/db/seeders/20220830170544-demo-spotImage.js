bb0a93fd14aec9b420073a2b1c93f881e9785cdf
Eliza <elizawimberly@gmail.com>
2022-08-30 10:21:12 -0700

    created and seeded data for spotImages table

---
+++ backend/db/seeders/20220830170544-demo-spotImage.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "SpotImages",
+      [
+        {
+          spotId: 1,
+          url: "https://commons.wikimedia.org/wiki/File:Ennis_House_front_view_2005.jpg",
+          preview: true,
+        },
+        {
+          spotId: 2,
+          url: "https://commons.wikimedia.org/wiki/File:Hollyhock_House_4.jpg",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "https://commons.wikimedia.org/wiki/File:Samuel-Novarro_House_West_Facade_2.jpg",
+          preview: true,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("SpotImages", null, {});
+  },
+};
