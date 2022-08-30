e571f8df7da7a214b318ac76c800bce1361d3011
Eliza <elizawimberly@gmail.com>
2022-08-30 10:03:22 -0700

    created and seeded data for spots table

--- backend/db/seeders/20220830153812-demo-spot.js
+++ backend/db/seeders/20220830153812-demo-spot.js
@@ -11,23 +11,50 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
-    // await queryInterface.bulkInsert;
-    // "Spots",
-    //   [
-    //     {
-    //       ownerId: DataTypes.INTEGER,
-    //       address: DataTypes.STRING,
-    //       city: DataTypes.STRING,
-    //       state: DataTypes.STRING,
-    //       country: DataTypes.STRING,
-    //       lat: DataTypes.DECIMAL,
-    //       lng: DataTypes.DECIMAL,
-    //       name: DataTypes.STRING,
-    //       description: DataTypes.STRING,
-    //       price: DataTypes.DECIMAL,
-    //     },
-    //   ],
-    //   {}();
+    await queryInterface.bulkInsert(
+      "Spots",
+      [
+        {
+          ownerId: 1,
+          address: "345 Chestnut Street",
+          city: "Miami",
+          state: "Florida",
+          country: "US",
+          lat: 34.1253618,
+          lng: -118.287994,
+          name: "Ennis House",
+          description: '"Mayan Revival-style house built by Frank Lloyd Wright',
+          price: 200,
+        },
+        {
+          ownerId: 1,
+          address: "4800 Hollywood Blvd",
+          city: "Los Angeles",
+          state: "California",
+          country: "US",
+          lat: 34.10013,
+          lng: -118.297994,
+          name: "Hollyhock House",
+          description:
+            '"Historical landmark built by Frank Lloyd Wright located in a city park',
+          price: 120,
+        },
+        {
+          ownerId: 3,
+          address: "2255 Verde Oak Dr",
+          city: "San Francisco",
+          state: "California",
+          country: "US",
+          lat: 36.10013,
+          lng: -150.297994,
+          name: "Samuel Novarro House",
+          description:
+            '"Architectural landmark built by Lloyd Wright designed in 1928',
+          price: 250,
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
