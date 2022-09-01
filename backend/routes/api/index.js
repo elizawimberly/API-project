eeda1b0a8f8a29fc33e194dfc88f6853d65dfc19
Eliza <elizawimberly@gmail.com>
2022-09-01 13:38:16 -0700

    added for endpoint for reviews router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
+const reviewsRouter = require("./reviews.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 //added requireAuth import based on alec's walthru
@@ -22,6 +23,8 @@ router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 
 router.use("/spots", spotsRouter);
+
+router.use("/reviews", reviewsRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const {
+  Booking,
+  Review,
+  ReviewImage,
+  Spot,
+  SpotImage,
+  User,
+} = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+// const { sequelize } = require("../../db/models/index");
+const sequelize = require("sequelize");
+const { Op } = require("sequelize");
+
+const router = express.Router();
+
+router.use(express.json());
+
+//
+//
+//
+//
+//CREATE AN IMAGE FOR A REVIEW
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  let reviewId = req.params.reviewId;
+
+  const { url } = req.body;
+
+  let reviewImage = await ReviewImage.create({
+    reviewId,
+    url,
+  });
+
+  res.json({
+    id: reviewImage.id,
+    url: reviewImage.url,
+  });
+});
+
+//
+//
+//
+//
+//GET REVIEWS OF CURRENT USER
+
+//
+//
+//
+//
+//end
+module.exports = router;
