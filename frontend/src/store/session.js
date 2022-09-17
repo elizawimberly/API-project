8a1d0be62fa86246c3ea8ccd2ffacd62f8ad65b8
Eliza <elizawimberly@gmail.com>
2022-09-17 16:24:45 -0700

    phase 2 complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -50,7 +50,23 @@ export const login = (user) => async (dispatch) => {
 export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch("/api/session");
   const data = await response.json();
-  console.log("data", data);
+  dispatch(setUser(data));
+  return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
   dispatch(setUser(data));
   return response;
 };
