06cc95deafe19edeb320b4d31a90c1d79d561de0
Eliza <elizawimberly@gmail.com>
2022-09-19 12:42:17 -0700

    updated package.json for implement heroku deployment instructions from auth-me

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,24 +1,10 @@
 // backend/routes/index.js
 const express = require("express");
-const apiRouter = require("./api");
 const router = express.Router();
-
-// router.get("/hello/world", function (req, res) {
-//   res.cookie("XSRF-TOKEN", req.csrfToken());
-//   res.send("Hello World!");
-// });
-
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
+const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
-//add mod5 code here
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === "production") {
@@ -26,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
@@ -37,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "build", "index.html")
     );
   });
@@ -47,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
