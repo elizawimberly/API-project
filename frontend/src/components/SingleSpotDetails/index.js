fc13a2d1b10d8ff8962bd82a6daf9312641d3a43
Eliza <elizawimberly@gmail.com>
2022-09-20 13:08:15 -0700

    properly implementing state for getSpotById

--- frontend/src/components/SingleSpotDetails/index.js
+++ frontend/src/components/SingleSpotDetails/index.js
@@ -3,16 +3,21 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getAllSpots } from "../../store/spots";
+import { getSpotById } from "../../store/spots";
 
 const SingleSpotDetails = () => {
   const dispatch = useDispatch();
   const allSpots = useSelector((state) => state.spots.allSpots);
 
-  useEffect(() => {
-    dispatch(getAllSpots());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getAllSpots());
+  // }, [dispatch]);
 
   const { spotId } = useParams();
+
+  useEffect(() => {
+    dispatch(getSpotById(spotId));
+  }, [dispatch, spotId]);
 
   console.log("allSpots", allSpots);
   console.log("spotId", spotId);
@@ -22,7 +27,7 @@ const SingleSpotDetails = () => {
 
   console.log("targetSpot", targetSpot);
 
-  return <div></div>;
+  return <div>airbnb</div>;
 };
 
 export default SingleSpotDetails;
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,5 +1,6 @@
 //TYPES
 const GET_ALL_SPOTS = "spots/read";
+const GET_SPOT_BY_ID = "spots/id/read";
 const CREATE_SPOT = "spots/create";
 const UPDATE_SPOT = "spots/update";
 const DELETE_SPOT = "spots/delete";
@@ -8,6 +9,13 @@ const DELETE_SPOT = "spots/delete";
 const getAllSpotsAction = (payload) => {
   return {
     type: GET_ALL_SPOTS,
+    payload,
+  };
+};
+
+const getSpotByIdAction = (payload) => {
+  return {
+    type: GET_SPOT_BY_ID,
     payload,
   };
 };
@@ -22,12 +30,21 @@ export const getAllSpots = () => async (dispatch) => {
   }
 };
 
+export const getSpotById = (spotId) => async (dispatch) => {
+  const res = await fetch(`/api/spots/${spotId}`);
+
+  if (res.ok) {
+    const spot = await res.json();
+    dispatch(getSpotByIdAction(spot));
+  }
+};
+
 //ESTABLISH STATE
 // const initialState = { spots: {} };
-const initialState = { allSpots: {} };
+const initialState = { allSpots: {}, singleSpot: {} };
 
 const spotsReducer = (state = initialState, action) => {
-  let newState;
+  // let newState;
   switch (action.type) {
     case GET_ALL_SPOTS:
       // console.log(state);
@@ -39,6 +56,15 @@ const spotsReducer = (state = initialState, action) => {
         (spot) => (newState.allSpots[spot.id] = spot)
       );
       return newState;
+    case GET_SPOT_BY_ID:
+      const newStateObj = { ...state };
+      console.log(
+        "action.payload from getspotbyid reducer case",
+        action.payload
+      );
+      newStateObj.singleSpot = { ...action.payload };
+      return newStateObj;
+
     // case REMOVE_USER:
     //   newState = Object.assign({}, state);
     //   newState.user = null;
